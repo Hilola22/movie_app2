6fd90ffa9e5b1ac83ef16b2dfdd6b30a002717d2
Hilola22 <hilolaorazaliyeva2203@gmail.com>
2025-09-25 16:55:49 +0500

    second commit

--- src/features/login/ui/Login.tsx
+++ src/features/login/ui/Login.tsx
@@ -3,8 +3,8 @@ import { CiLogin } from "react-icons/ci";
 
 export const Login = memo(() => {
   return (
-    <button className="flex items-center gap-2 py-[8px] px-[26px] text-white bg-[#C61F1F] rounded-xl">
-      <span>Enter</span>
+    <button className="flex items-center gap-2 py-[8px] px-[26px] text-white bg-[#C61F1F] hover:bg-[#9e2020]  rounded-xl">
+      <span>Login</span>
       <CiLogin size={22} />
     </button>
   );
